ea02a6285746623cd7d01831886409982240cfc5
Michael Hopkins <michael@appitventures.com>
2015-02-14 06:42:12 -0700

    first

---
+++ src/Providers/GamesServiceProvider.php
@@ -0,0 +1,17 @@
+<?php namespace Hopkins\SlackAgainstHumanity\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class GamesServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
