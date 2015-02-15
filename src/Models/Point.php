b3e14f3c58d7f063ea5d5ce1f867f9e20fe9eff6
Michael Hopkins <michael@appitventures.com>
2015-02-14 20:00:23 -0700

    adding watson validation

--- src/Models/Point.php
+++ src/Models/Point.php
@@ -4,7 +4,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Point extends Model
 {
-    protected $fillable = ['player_id','reason','modifier'];
+    use ValidatingTrait;
+
+    protected $rules = [
+        'for'   => 'required',
+        'modifier'    => 'required',
+        'reason' => 'string',
+        'given_by' => 'string'
+    ];
+    protected $guard = ['id'];
 
     public function player(){
         return $this->belongsTo(Player::class);
