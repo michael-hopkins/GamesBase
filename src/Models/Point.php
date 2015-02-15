041d44bd22493445deee30e7762ef5289e58e847
Michael Hopkins <michael@appitventures.com>
2015-02-14 21:17:56 -0700

    adding phpdocblocks

--- src/Models/Point.php
+++ src/Models/Point.php
@@ -3,6 +3,24 @@
 use Illuminate\Database\Eloquent\Model;
 use Watson\Validating\ValidatingTrait;
 
+/**
+ * Hopkins\GamesBase\Models\Point
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property integer $point
+ * @property string $for
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read Player $player
+ * @property-read \Illuminate\Database\Eloquent\Collection|\$related[] $morphedByMany
+ * @method static \Illuminate\Database\Query\Builder|\Hopkins\GamesBase\Models\Point whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Hopkins\GamesBase\Models\Point whereUserId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Hopkins\GamesBase\Models\Point wherePoint($value)
+ * @method static \Illuminate\Database\Query\Builder|\Hopkins\GamesBase\Models\Point whereFor($value)
+ * @method static \Illuminate\Database\Query\Builder|\Hopkins\GamesBase\Models\Point whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Hopkins\GamesBase\Models\Point whereUpdatedAt($value)
+ */
 class Point extends Model
 {
     use ValidatingTrait;
